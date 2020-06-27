6d73cb999615898d89318ddd53c9762c3e7412bf
houyongxu <houyongxu@126.com>
2020-06-27 21:06:35 +0800

    添加注释

--- src/common/AdminAuthControllers.php
+++ src/common/AdminAuthControllers.php
@@ -3,6 +3,12 @@
 namespace adminauth\common;
 use yii\web\Controller;
 
+/**
+ * 控制器基类
+ * @author houyongxu
+ * @date 2020-06-27
+ *
+ */
 class AdminAuthController extends Controller
 {
 	public function init()
--- src/controllers/UserControllers.php
+++ src/controllers/UserControllers.php
@@ -3,6 +3,12 @@
 namespace adminauth\controllers;
 use adminauth\common\AdminAuthController;
 
+/**
+ * 用户类
+ * @author houyongxu
+ * @date 2020-06-27
+ *
+ */
 class UserController extends AdminAuthController
 {
 	public function init()

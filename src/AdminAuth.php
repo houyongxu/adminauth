d348b23fdfd341ff8e94b6bb2edca8b6a4c25024
houyongxu <houyongxu@126.com>
2020-06-27 20:28:02 +0800

    auth

---
+++ src/AdminAuth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace adminauth;
+
+/**
+ * asfd module definition class
+ */
+class AdminAuth extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'adminauth\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ src/controllers/AdminAuthControllers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace adminauth\controllers;
+use yii\web\Controller;
+
+class AdminAuthController extends Controller
+{
+	public function init()
+	{
+		parent::init();
+	}
+}
